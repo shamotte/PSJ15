4b8896589c072474b652d7b1363dceb68f384509
Zed-z <71879051+Zed-z@users.noreply.github.com>
2024-07-21 00:16:09 +0200

    Main menu and title

--- scenes/Menu.gd
+++ scenes/Menu.gd
@@ -1,13 +1,5 @@
 extends Control
 
-func _on_alchemy_pressed():
-	get_tree().change_scene_to_file("res://scenes/crafting.tscn")
-
-
-func _on_adventure_pressed():
-	get_tree().change_scene_to_file("res://adventure/scenes/world.tscn")
-
-
 func _on_exit_pressed():
 	get_tree().quit()
 
