58d576654124fea63185956c410dc68e2af7f5f8
Zed-z <71879051+Zed-z@users.noreply.github.com>
2024-07-31 00:14:21 +0200

    Music, menu and game over

---
+++ globals/music_manager.gd
@@ -0,0 +1,23 @@
+extends Node
+
+func _ready():
+	$ExplorationMusic.play()
+	$CraftingMusic.play()
+	$ExplorationMusic.stream_paused = true
+	$CraftingMusic.stream_paused = true
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var world_manager = get_tree().get_root().get_node("WorldManager")
+	
+	if world_manager != null:
+		match world_manager.state:
+			
+			CombinedScenes.STATE.WORLD:
+				$ExplorationMusic.stream_paused = false
+				$CraftingMusic.stream_paused = true
+				
+			CombinedScenes.STATE.CRAFTING:
+				$ExplorationMusic.stream_paused = true
+				$CraftingMusic.stream_paused = false
+
