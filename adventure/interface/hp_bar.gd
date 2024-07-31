8fd7be5682f8b4ae727fa2c7403bc37bb95c2083
Liniux <153652735+Lin1ux@users.noreply.github.com>
2024-07-31 11:33:50 +0200

    something

--- adventure/interface/hp_bar.gd
+++ adventure/interface/hp_bar.gd
@@ -9,10 +9,11 @@ var hp_component : Node
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	hp_component = character.get_node("CompHealth")
-	for i in range(hp_component.get_current_hp()):
-		var hp_icon = hp_scene.instantiate()
-		$VBoxContainer.add_child(hp_icon)
-	hp_component.connect("hp_changed",refresh_hp)
+	refresh_hp()
+	#for i in range(hp_component.get_current_hp()):
+	#	var hp_icon = hp_scene.instantiate()
+	#	$VBoxContainer.add_child(hp_icon)
+	#hp_component.connect("hp_changed",refresh_hp)
 	
 
 
